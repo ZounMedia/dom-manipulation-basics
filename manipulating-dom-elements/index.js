e4171ab4716dc724bad5700bd85c2c4b6a5df5b8
Romaric Zounlome <rzounlom@gmail.com>
2024-02-20 07:13:49 -0500

    chore: update dom selection and manipulation examples

--- manipulating-dom-elements/index.js
+++ manipulating-dom-elements/index.js
@@ -1,24 +1,24 @@
 // Method 1: innerHTML
 let innerHTMLResult = document.getElementById("innerHTMLResult");
 // console.log({ innerHTMLResult });
-innerHTMLResult.innerHTML = "<strong>Modified</strong> Content with innerHTML";
+// innerHTMLResult.innerHTML = "<strong>Modified</strong> Content with innerHTML";
 
 // Method 2: textContent
-let textContentResult = document.getElementById("textContentResult");
+// let textContentResult = document.getElementById("textContentResult");
 // console.log({ textContentResult });
 
-textContentResult.textContent = "Modified Content with textContent";
+// textContentResult.textContent = "Modified Content with textContent";
 
-// // Method 3: setAttribute
-let setAttributeResult = document.getElementById("setAttributeResult");
-setAttributeResult.setAttribute("class", "modifiedClass");
+// Method 3: setAttribute
+// let setAttributeResult = document.getElementById("setAttributeResult");
+// setAttributeResult.setAttribute("class", "modifiedClass");
 
-// // Method 4: style
-let styleResult = document.getElementById("styleResult");
-styleResult.style.color = "blue";
-styleResult.style.fontSize = "20px";
+// Method 4: style
+// let styleResult = document.getElementById("styleResult");
+// styleResult.style.color = "blue";
+// styleResult.style.fontSize = "20px";
 
-// // Method 5: classList
-let classListResult = document.getElementById("classListResult");
-classListResult.classList.add("newClass");
+// Method 5: classList
+// let classListResult = document.getElementById("classListResult");
+// classListResult.classList.add("newClass");
 // classListResult.classList.remove("originalClass");
