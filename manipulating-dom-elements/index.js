2be44206afea705f8e2c113aff6a3a9b2ab88bdd
Romaric Zounlome <rzounlom@gmail.com>
2024-03-14 21:14:14 -0400

    chore: update demo

--- manipulating-dom-elements/index.js
+++ manipulating-dom-elements/index.js
@@ -1,29 +1,30 @@
 // Method 1: innerHTML
 let innerHTMLResult = document.getElementById("innerHTMLResult");
-console.log({ innerHTMLResult });
-innerHTMLResult.innerHTML = "<strong>Modified</strong> Content with innerHTML";
+console.log(innerHTMLResult);
+// innerHTMLResult.innerHTML = "<strong>Modified</strong> Content with innerHTML";
 
 // Method 2: textContent
 let textContentResult = document.getElementById("textContentResult");
-console.log({ textContentResult });
+// console.log({ textContentResult });
 
-textContentResult.textContent = "Modified Content with textContent";
+// textContentResult.textContent = "Modified Content with textContent";
 
 // Method 3: setAttribute
 let setAttributeResult = document.getElementById("setAttributeResult");
-setAttributeResult.setAttribute("class", "modifiedClass");
+// setAttributeResult.setAttribute("class", "modifiedClass");
 
-console.log({ setAttributeResult });
+// console.log({ setAttributeResult });
 
 // Method 4: style
 let styleResult = document.getElementById("styleResult");
-console.log({ styleResult });
-styleResult.style.color = "blue";
-styleResult.style.fontSize = "20px";
+// console.log({ styleResult });
+// styleResult.style.color = "blue";
+// styleResult.style.fontSize = "20px";
 
 // Method 5: classList
 let classListResult = document.getElementById("classListResult");
-console.log({ classListResult });
-classListResult.classList.add("newClass");
-classListResult.classList.remove("originalClass");
-console.log({ classListResult });
+// console.log({ classListResult });
+// classListResult.classList.remove("lastOriginalClass");
+// classListResult.classList.add("newClass");
+
+// console.log({ classListResult });
--- selecting-dom-elelemnts/index.js
+++ selecting-dom-elelemnts/index.js
@@ -1,31 +1,31 @@
 // Method 1: getElementById
 let elementById = document.getElementById("byIdResult");
 console.log("**elementById**", elementById);
-elementById.textContent = "Element accessed by ID: " + elementById.id;
+// elementById.textContent = "Element accessed by ID: " + elementById.id;
 
 // Method 2: getElementsByClassName: returnes an array of elements found with the specified class name
 let byClassNameResultTotal = document.getElementsByClassName(
   "byClassNameResultTotal"
 );
 
-console.log("**elementsByClassName**", byClassNameResultTotal);
+// console.log("**byClassNameResultTotal**", byClassNameResultTotal);
 
 let byClassNameResult = document.getElementsByClassName("byClassNameResult");
-console.log("**elementsByClassNameResult**", byClassNameResult);
+// console.log("**elementsByClassNameResult**", byClassNameResult);
 
 //updating text content of the byClassNameResultTotal element (changing the first element using array index of 0)
-byClassNameResultTotal[0].textContent =
-  "Elements accessed by Class Name: " + byClassNameResult.length + " found";
+// byClassNameResultTotal[0].textContent =
+//   "Elements accessed by Class Name: " + byClassNameResult.length + " found";
 
 //spread in the array of elements found by the query and update each conent
-[...byClassNameResult].forEach(
-  (element, idx) =>
-    (element.textContent = `Element ${idx} text content modified`)
-);
+// [...byClassNameResult].forEach(
+//   (element, idx) =>
+//     (element.textContent = `Element ${idx} text content modified`)
+// );
 
 // Method 3: getElementsByTagName
 let elementsByTagName = document.getElementsByTagName("div");
-console.log("**elementsByTagName**", elementsByTagName);
+// console.log("**elementsByTagName**", elementsByTagName);
 
 //updating text content of the byTagNameResult element
 document.getElementById("byTagNameResult").textContent =
@@ -33,8 +33,8 @@ document.getElementById("byTagNameResult").textContent =
 
 // Method 4: querySelector: returns one element, and querySelectorAll: finds all elements based on the css selector of the element and returns the results in an array
 let elementsBySelectorAll = document.querySelectorAll(".section");
-console.log("**elementsBySelector**", elementsBySelectorAll);
+// console.log("**elementsBySelector**", elementsBySelectorAll);
 
 //updating text content of the bySelectorResult element
-document.getElementById("bySelectorResult").textContent =
-  "Number of elements with class 'section': " + elementsBySelectorAll.length;
+// document.getElementById("bySelectorResult").textContent =
+//   "Number of elements with class 'section': " + elementsBySelectorAll.length;
