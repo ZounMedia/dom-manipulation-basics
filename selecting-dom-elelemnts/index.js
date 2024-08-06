58b0eff22bd2868e7fdfabfd730caf2558ecf934
Romaric Zounlome <rzounlom@gmail.com>
2024-08-06 18:31:25 -0400

    minor update

--- selecting-dom-elelemnts/index.js
+++ selecting-dom-elelemnts/index.js
@@ -24,16 +24,18 @@ let byClassNameResult = document.getElementsByClassName("byClassNameResult");
 // );
 
 // Method 3: getElementsByTagName
-let elementsByTagName = document.getElementsByTagName("div");
+// let elementsByTagName = document.getElementsByTagName("div");
 // console.log("**elementsByTagName**", elementsByTagName);
 
 //updating text content of the byTagNameResult element
-document.getElementById("byTagNameResult").textContent =
-  "Number of div elements: " + elementsByTagName.length;
+// document.getElementById("byTagNameResult").textContent =
+//   "Number of div elements: " + elementsByTagName.length;
 
 // Method 4: querySelector: returns one element, and querySelectorAll: finds all elements based on the css selector of the element and returns the results in an array
-let elementsBySelectorAll = document.querySelectorAll(".section");
-// console.log("**elementsBySelector**", elementsBySelectorAll);
+// let elementsBySelector = document.querySelector(".section");
+// let elementsBySelectorAll = document.querySelectorAll(".section");
+// console.log("**elementsBySelector**", elementsBySelector);
+// console.log("**elementsBySelectorAll**", elementsBySelectorAll);
 
 //updating text content of the bySelectorResult element
 // document.getElementById("bySelectorResult").textContent =
